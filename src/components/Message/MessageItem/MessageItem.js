5970ae9c7f54971e8f9e557a179e63bbc839c6eb
Anton Sharudylo <a.sharudylo@gmail.com>
2022-08-22 21:14:18 +0300

    added changes

--- src/components/Message/MessageItem/MessageItem.js
+++ src/components/Message/MessageItem/MessageItem.js
@@ -9,20 +9,20 @@ import './MessageItem.scss'
 const { accent, secondary } = colors;
 
 const MessageItem = ({ message, imageUrl, contactId }) => {
-  const isownmessage = (message.from !== contactId).toString();
+  const isOwnMessage = (message.from !== contactId);
   return (
     <Flex style={{ flexDirection: "row" }}>
       {
-        !isownmessage
+        !isOwnMessage
           ? <Avatar imageUrl={imageUrl} />
           : null
       }
       <Flex>
-        <div className="message__item" style={isownmessage ? { alignItems: "flex-end" } : { alignItems: "flex-start" }} isownmessage={isownmessage}>
-          <div className="message__text" style={isownmessage ? { backgroundColor: accent } : { backgroundColor: secondary }}>
-            <p className="message__value" style={isownmessage ? { color: "black" } : { color: "white" }}>{message.value}</p>
+        <div className="message__item" style={isOwnMessage ? { alignItems: "flex-end" } : { alignItems: "flex-start" }}>
+          <div className="message__text" style={isOwnMessage ? { backgroundColor: accent } : { backgroundColor: secondary }}>
+            <p className="message__value" style={isOwnMessage ? { color: "black" } : { color: "white" }}>{message.value}</p>
           </div>
-          <div className="message__time" style={isownmessage ? { textAlign: "right" } : { textAlign: "left" }}>
+          <div className="message__time" style={isOwnMessage ? { textAlign: "right" } : { textAlign: "left" }}>
             <p>
               {
                 moment(message.createdDate).format("MM/DD/YY LT")
